d4fafdc84be0507f0009e35029d8bc68e7ed613e
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2024-07-19 12:32:01 -0500

    Changed everythign, started anew

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,29 +1,23 @@
-import cartLogo from '../../assets/icons/cart-outline.svg';
 import NavStyles from './Navbar.module.css';
 
 function Navbar() {
   return (
     <nav className={NavStyles['nav-container']}>
       <ul className={NavStyles['unordered-list']}>
-        <li
-          className={`${NavStyles['list-items']} ${NavStyles['center-item']}`}
-        >
-          <a className={NavStyles.links} href='/'>
+        <li className={NavStyles['list-item']}>
+          <a href='/home' className={NavStyles.anchor}>
             Home
           </a>
         </li>
-        <li
-          className={`${NavStyles['center-item']} ${NavStyles['list-items']} ${NavStyles['active-link']}`}
-        >
-          <a className={NavStyles.links} href='/favorites'>
+        <li className={NavStyles['list-item']}>
+          <a href='/favorites' className={NavStyles.anchor}>
             Favorites
           </a>
         </li>
-        <li
-          className={`${NavStyles['list-items']} ${NavStyles['center-item']} ${NavStyles['active-link']}`}
-        >
-          <a className={NavStyles['cart-link']} href='/cart'>
-            <img src={cartLogo} alt='Cart' className={NavStyles['cart-icon']} />
+        <li className={NavStyles['list-item']}>
+          <div className='spacer'></div>
+          <a href='/shopping-cart' className={NavStyles.anchor}>
+            <ion-icon name='bag'></ion-icon>
           </a>
         </li>
       </ul>
