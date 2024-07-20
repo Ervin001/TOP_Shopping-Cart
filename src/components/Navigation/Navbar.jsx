f0610b660587da779263fca61d53006fc4c8bcd0
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2024-07-19 23:53:20 -0500

    Nav is working, no logic for react router yet

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,23 +1,44 @@
 import NavStyles from './Navbar.module.css';
+import { useState } from 'react';
 
 function Navbar() {
+  const [activeLink, setActiveLink] = useState('home');
+
   return (
     <nav className={NavStyles['nav-container']}>
       <ul className={NavStyles['unordered-list']}>
-        <li className={NavStyles['list-item']}>
-          <a href='/home' className={NavStyles.anchor}>
+        <li
+          className={`${NavStyles['list-item']} ${
+            activeLink === 'home' ? NavStyles['list-item-active'] : ''
+          }`}
+          onClick={() => setActiveLink('home')}
+        >
+          <a href='#' className={NavStyles.anchor}>
             Home
           </a>
         </li>
-        <li className={NavStyles['list-item']}>
-          <a href='/favorites' className={NavStyles.anchor}>
+        <li
+          className={`${NavStyles['list-item']} ${
+            activeLink === 'favorites' ? NavStyles['list-item-active'] : ''
+          }`}
+          onClick={() => setActiveLink('favorites')}
+        >
+          <a href='#' className={NavStyles.anchor}>
             Favorites
           </a>
         </li>
-        <li className={NavStyles['list-item']}>
-          <div className='spacer'></div>
-          <a href='/shopping-cart' className={NavStyles.anchor}>
-            <ion-icon name='bag'></ion-icon>
+        <li
+          className={`${NavStyles['list-item']} ${
+            activeLink === 'cart' ? NavStyles['list-item-active'] : ''
+          }`}
+          onClick={() => setActiveLink('cart')}
+        >
+          <a href='#' className={NavStyles.anchor}>
+            <ion-icon
+              name='bag'
+              size='large'
+              className={`${NavStyles['ion-icon']}`}
+            ></ion-icon>
           </a>
         </li>
       </ul>
