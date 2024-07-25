14af22d44b4a944b3d3e181c51186895ee3e89ab
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2024-07-25 18:21:57 -0500

    Style: Styled the product items, changed some of the structure in the html

--- src/components/Home/Card/Card.jsx
+++ src/components/Home/Card/Card.jsx
@@ -2,16 +2,26 @@ import NavStyles from './Card.module.css';
 
 function Card({ product }) {
   return (
-    <>
-      <h2>{product.title}</h2>
-      <h4>{product.price}</h4>
-      <p>{product.description}</p>
-      <p>{product.category}</p>
-      {/* <img src={product.image}></img> */}
-      <h3>Rating</h3>
-      <span>{product.rating.rate}</span>
-      <span>{product.rating.count}</span>
-    </>
+    <a href='#' className={`${NavStyles['product-item']}`}>
+      <div className={`${NavStyles['product-img-container']}`}>
+        <img className={`${NavStyles['item-img']}`} src={product.image}></img>
+      </div>
+      <h2 className={`${NavStyles['product-title']}`}>{product.title}</h2>
+      <div className={`${NavStyles['product-extra-info']}`}>
+        <div className={`${NavStyles['btn-container']}`}>
+          <button className={`${NavStyles['add-to-cart-btn']}`}>
+            <ion-icon name='cart'></ion-icon>
+            <span>Add to cart</span>
+          </button>
+        </div>
+        <h4 className={`${NavStyles['product-price']}`}>{product.price}</h4>
+      </div>
+      {/* <p>{product.description}</p> */}
+      {/* <p>{product.category}</p> */}
+      {/* <h3>Rating</h3> */}
+      {/* <span>{product.rating.rate}</span>zs */}
+      {/* <span>{product.rating.count}</span> */}
+    </a>
   );
 }
 
