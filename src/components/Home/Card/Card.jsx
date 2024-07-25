e3a81d740795123549ad9fd306cbef65544aeb6c
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2024-07-25 18:48:25 -0500

    function added for truncating the title from the product

--- src/components/Home/Card/Card.jsx
+++ src/components/Home/Card/Card.jsx
@@ -1,12 +1,21 @@
 import NavStyles from './Card.module.css';
 
+function truncateTitle(title) {
+  const titleLength = 40;
+  return title.length > titleLength
+    ? `${title.substring(0, titleLength)}...`
+    : title;
+}
+
 function Card({ product }) {
   return (
     <a href='#' className={`${NavStyles['product-item']}`}>
       <div className={`${NavStyles['product-img-container']}`}>
         <img className={`${NavStyles['item-img']}`} src={product.image}></img>
       </div>
-      <h2 className={`${NavStyles['product-title']}`}>{product.title}</h2>
+      <h2 className={`${NavStyles['product-title']}`}>
+        {truncateTitle(product.title)}
+      </h2>
       <div className={`${NavStyles['product-extra-info']}`}>
         <div className={`${NavStyles['btn-container']}`}>
           <button className={`${NavStyles['add-to-cart-btn']}`}>
@@ -14,7 +23,7 @@ function Card({ product }) {
             <span>Add to cart</span>
           </button>
         </div>
-        <h4 className={`${NavStyles['product-price']}`}>{product.price}</h4>
+        <h4 className={`${NavStyles['product-price']}`}>${product.price}</h4>
       </div>
       {/* <p>{product.description}</p> */}
       {/* <p>{product.category}</p> */}
